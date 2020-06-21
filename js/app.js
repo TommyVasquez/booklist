cda15420d77262521c295f6277bace8686cc179d
TommyAyrton <tommyvasquezr@gmail.com>
2020-06-20 22:09:42 -0500

    actualzacion

--- js/app.js
+++ js/app.js
@@ -16,12 +16,12 @@ class UI {
             {
                 title: 'Book One',
                 author: 'John Doe',
-                isbn = '34343434'
+                isbn: '34343434'
             },
             {
                 title: 'Book Two',
                 author: 'Jane Doe',
-                isbn = '45454545'
+                isbn: '45454545'
             }
         ]; 
         
@@ -32,16 +32,46 @@ class UI {
     }
 
     static addBookToList(book) {
-        const li = document.querySelector('#book-list');
+        const list = document.querySelector('#book-list');
         const row = document.createElement('tr');
-        li.appendChild(row);
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+        list.appendChild(row);
+    }
+
+    static clearFields(){
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
     }
 }
 
 // Store Clas: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
 
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Addd book to UI
+    UI.addBookToList(book);
+
+    // Clear fields
+    UI.clearFields();
+});
 // Event: Remove s Book
